1f949b15c4b4fb09dac94637422568123988ec3d
Jinyue Xia <xiajinyue@gmail.com>
2017-02-18 00:13:09 -0500

    play with reduce

--- reduce/reduce-basic.js
+++ reduce/reduce-basic.js
@@ -12,3 +12,43 @@ var totalAmount = orders.reduce(function(sum, order) {
 }, 0); // 0 is the initial value (starting point)
 
 console.log(totalAmount);
+
+var endorsements = [
+  { skill: 'javascript', user: 'Chad' },
+  { skill: 'javascript', user: 'Bill' },
+  { skill: 'css', user: 'Sue' },
+  { skill: 'javascript', user: 'Sue' },
+  { skill: 'css', user: 'Bill' },
+  { skill: 'html', user: 'Sue' }
+];
+
+// Result
+
+//[
+//  { skill: 'javascript', user: [ 'Chad', 'Bill', 'Sue' ], count: 3 },
+//  { skill: 'css', user: [ 'Sue', 'Bill' ], count: 2 },
+//  { skill: 'html', user: [ 'Sue' ], count: 1 }
+//];
+
+var results = endorsements.reduce((result, endorsement) => {
+  var newSkill = true;
+  result.forEach((res) => {
+    if (res.skill === endorsement.skill) {
+      res.user.push(endorsement.user);
+      res.count++;
+      newSkill = false;
+    }
+  });
+
+  if (newSkill) {
+    result.push({
+      skill: endorsement.skill,
+      user: [endorsement.user],
+      count: 1
+    });
+  }
+
+  return result;
+}, []);
+
+console.log(results);
--- reduce/reduce.js
+++ reduce/reduce.js
@@ -1,3 +1,22 @@
+// mark john waffle 80 2
+// mark john blender 200 1
+// mark john knife 10 4
+// Nick Smith waffle 80 1
+// Nick Smith knife 10 2
+// Nick Smith pot 20 3
+
+// output:
+// {
+//   mark:
+//    [ { name: 'john', price: 'waffle', quantity: '80' },
+//      { name: 'john', price: 'blender', quantity: '200' },
+//      { name: 'john', price: 'knife', quantity: '10' } ],
+//   Nick:
+//    [ { name: 'Smith', price: 'waffle', quantity: '80' },
+//      { name: 'Smith', price: 'knife', quantity: '10' },
+//      { name: 'Smith', price: 'pot', quantity: '20' } ]
+// }
+
 var fs = require('fs');
 
 var output = fs.readFileSync('data.txt', 'utf8')
